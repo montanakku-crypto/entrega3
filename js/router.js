771a5e5dac1e3ad57c9456e62bbf62785ec903fb
montanakku-crypto <montana.kku2000@gmail.com>
2025-11-01 21:41:02 -0300

    Create router.js

---
+++ js/router.js
@@ -0,0 +1,7 @@
+export function showPage(pageId) {
+    document.querySelectorAll(".page").forEach(page =>
+        page.classList.add("hidden")
+    )
+
+    document.getElementById(pageId).classList.remove("hidden")
+}
